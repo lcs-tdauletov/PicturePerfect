4cf4748572315b111ee1cc3e4acd0808fd223afd
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2018-05-22 19:56:19 -0400

    Yes. Documentation for my main teacher Gordon

--- PicturePerfect/main.swift
+++ PicturePerfect/main.swift
@@ -52,17 +52,47 @@ for counter in 1...countOfPhotoArrangementsToBeConsidered {
     guard let numberOfPhotos = Int(givenInput) else {
         continue
     }
-    
+    // Simply skip to the next iteration. Sure
+    if numberOfPhotos > 65_000 || numberOfPhotos < 0 {
+        continue
+    }
     // Implement the rest of your logic here...
+    
+    
+    
+    
+    
+    
+    
+    // First of all, the declaration of the crucial element: the generic side.
+    // Note that the problem setting does not mention in which order the sides need to be listed
     var oneSide = 0
+    // Here is where fun begins: the extremely embarassing code now sorts out the plausible
+    // values for the first side.
+    
     var arrayOfSides : [Int] = []
     for i in 1...numberOfPhotos {
         if numberOfPhotos % i == 0 {
             arrayOfSides.append(i)
             
         }
+        // I figured that the minimum perimeter in the shapes that have more than one possible arrangement
+        // is obtained when the sides have the values close to the square root of the inputted number of photos
+        // But that is irrelevent. All you need to know is that for the case when we have 12 photos
+        // Out of all possibilities( 1 - 12, 3 - 4, 4 - 3, 6 - 2, 12 - 1)
+        // the middle arrangements will have the least perimeter.
+        // this part required thinking for quite a time
+        // See, pondering is crucial when problem solving.
+        // These problems ain't jokes after all ::DD
+        
         oneSide = arrayOfSides[(Int(arrayOfSides.count - 1 ) / 2)]
     }
+    // This is just a wrap-up part. Like any other problem:
+    // 1. Some generic instructions(beginning of algorithm)
+    // 2. Major problem( middle game)
+    // 3. Mark droppers added for confusion but once you get though the middle game, this endgame isn't anything(e.g. calulate the sum of all possible Palindromes(after I found Palindromes themselves, I would usually simply ignore this part), find the maximum Pentagonal number, blah, blah, blah. You know.
+    // So yeah, I do hope you enjoyed this little discussion we had. Looking forward for whatever dares to approach me.
+    
     let otherSide = numberOfPhotos / oneSide
     let perimeter = 2 * otherSide + 2 * oneSide
     print("Minimum perimeter is \(perimeter) with dimensions \(otherSide) and \(oneSide)")
